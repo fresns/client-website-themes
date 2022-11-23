74a7aab8796ca4c32404336d891a040bb73d28ab
Jarvis <jarvis.okay@gmail.com>
2022-11-23 15:07:06 +0800

    chore: tips

--- ThemeFrame/commons/tips.blade.php
+++ ThemeFrame/commons/tips.blade.php
@@ -1,19 +1,14 @@
 @if (session('success'))
-    <div aria-live="polite" aria-atomic="true" class="position-fixed top-50 start-50 translate-middle" style="z-index:99">
-        <div class="toast show" role="alert" aria-live="assertive" aria-atomic="true">
-            <div class="toast-header">
-                <img src="/static/images/icon.png" width="20px" height="20px" class="rounded me-2" alt="Fresns">
-                <strong class="me-auto">Fresns</strong>
-                <small>200</small>
-                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-            </div>
-            <div class="toast-body">
-                {{ session('success') }}
+    <div aria-live="polite" aria-atomic="true" class="position-fixed top-50 start-50 translate-middle" style="z-index:9999">
+        <div class="toast show text-bg-success" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="d-flex">
+                <div class="toast-body">{{ session('success') }}</div>
+                <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
         </div>
     </div>
 @elseif (session('failure'))
-    <div aria-live="polite" aria-atomic="true" class="position-fixed top-50 start-50 translate-middle" style="z-index:99">
+    <div aria-live="polite" aria-atomic="true" class="position-fixed top-50 start-50 translate-middle" style="z-index:9999">
         <div class="toast show" role="alert" aria-live="assertive" aria-atomic="true">
             <div class="toast-header">
                 <img src="/static/images/icon.png" width="20px" height="20px" class="rounded me-2" alt="Fresns">
@@ -25,22 +20,17 @@
                 <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
             <div class="toast-body">
-                {{ session('failure') }}
+                {!! session('failure') !!}
             </div>
         </div>
     </div>
 @elseif ($errors->any())
-    <div aria-live="polite" aria-atomic="true" class="position-fixed top-50 start-50 translate-middle" style="z-index:99">
+    <div aria-live="polite" aria-atomic="true" class="position-fixed top-50 start-50 translate-middle" style="z-index:9999">
         @foreach ($errors->all() as $error)
-            <div class="toast show" role="alert" aria-live="assertive" aria-atomic="true">
-                <div class="toast-header">
-                    <img src="/static/images/icon.png" width="20px" height="20px" class="rounded me-2" alt="Fresns">
-                    <strong class="me-auto">Fresns</strong>
-                    <small>400</small>
-                    <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-                </div>
-                <div class="toast-body">
-                    {!! $error !!}
+            <div class="toast show text-bg-danger" role="alert" aria-live="assertive" aria-atomic="true">
+                <div class="d-flex">
+                    <div class="toast-body">{!! $error !!}</div>
+                    <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
                 </div>
             </div>
         @endforeach
