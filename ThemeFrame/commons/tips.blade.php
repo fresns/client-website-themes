2f940b5ec824e7344cefce5e491abd47602b9771
Jarvis <jarvis.okay@gmail.com>
2023-01-17 16:29:40 +0800

    chore(frame): $errors->any()

--- ThemeFrame/commons/tips.blade.php
+++ ThemeFrame/commons/tips.blade.php
@@ -24,15 +24,4 @@
             </div>
         </div>
     </div>
-@elseif ($errors->any())
-    <div aria-live="polite" aria-atomic="true" class="position-fixed top-50 start-50 translate-middle" style="z-index:9999">
-        @foreach ($errors->all() as $error)
-            <div class="toast show text-bg-danger" role="alert" aria-live="assertive" aria-atomic="true">
-                <div class="d-flex">
-                    <div class="toast-body">{!! $error !!}</div>
-                    <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
-                </div>
-            </div>
-        @endforeach
-    </div>
 @endif
