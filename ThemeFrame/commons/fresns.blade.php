e2f17e06174d4660ae7af3266ce7ad02f65dc942
Jarvis <jarvis.okay@gmail.com>
2023-02-03 02:12:44 +0800

    feat(frame): add loading style

--- ThemeFrame/commons/fresns.blade.php
+++ ThemeFrame/commons/fresns.blade.php
@@ -67,6 +67,15 @@
     {{-- Footer --}}
     @include('commons.footer')
 
+    {{-- Loading --}}
+    <div id="loading" class="position-fixed top-0 start-0 bottom-0 end-0" style="background-color:rgba(255, 255, 255, 0.8);z-index:2048;">
+        <div class="position-absolute top-50 start-50 translate-middle">
+            <div class="spinner-border text-secondary" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
+    </div>
+
     {{-- Switching Languages Modal --}}
     @if (fs_api_config('language_status'))
         <div class="modal fade" id="translate" tabindex="-1" aria-labelledby="translateModal" aria-hidden="true">
@@ -113,6 +122,21 @@
         window.siteIcon = "{{ fs_db_config('site_icon') }}";
         window.langTag = "{{ current_lang_tag() }}";
         window.hashtagShow = {{ fs_api_config('hashtag_show') }};
+
+        // loading
+        $(document).ready(function () {
+            const loading = $("#loading");
+
+            $('a[href^="http"]').on('click', function(event) {
+                event.preventDefault();
+                loading.show();
+                window.location = $(this).attr('href');
+            });
+
+            $(document).ajaxStop(function () {
+                loading.hide();
+            });
+        });
 
         // video play
         var videos = document.getElementsByTagName('video'); 
