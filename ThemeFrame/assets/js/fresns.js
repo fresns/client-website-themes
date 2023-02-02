dbfd6bb4f7ec4a536fbdef66f662b0fd5b072594
Jarvis <jarvis.okay@gmail.com>
2023-02-02 18:32:49 +0800

    chore: tip code

--- ThemeFrame/assets/js/fresns.js
+++ ThemeFrame/assets/js/fresns.js
@@ -99,7 +99,7 @@ window.tips = function (message, code = 200) {
 
     $('div.fresns-tips').prepend(html);
 
-    if (code == 0) {
+    if (code == 0 || code == 200 || code == 400) {
         setTimeoutToastHide();
     }
 };
