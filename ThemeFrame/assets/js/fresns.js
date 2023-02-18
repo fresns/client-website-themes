3f02fabff4e37757c11eb28179a05debab194685
Jarvis <jarvis.okay@gmail.com>
2023-02-18 11:36:00 +0800

    chore(frame): download file

--- ThemeFrame/assets/js/fresns.js
+++ ThemeFrame/assets/js/fresns.js
@@ -183,20 +183,26 @@ function fetchSendVerifyCode(type, useType, templateId, account, action, obj, co
 }
 
 // download file
-function downloadFile(data, fileName, type = 'text/plain') {
-    // Create an invisible A element
-    const a = document.createElement('a');
-    a.style.display = 'none';
-    document.body.appendChild(a);
-    // Set the HREF to a Blob representation of the data to be downloaded
-    a.href = window.URL.createObjectURL(new Blob([data], { type }));
-    // Use download attribute to set set desired file name
-    a.setAttribute('download', fileName);
-    // Trigger the download by simulating click
-    a.click();
-    // Cleanup
-    window.URL.revokeObjectURL(a.href);
-    document.body.removeChild(a);
+function downloadFile(url, fileName, mimeType) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'blob';
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            const blob = xhr.response;
+            const a = document.createElement('a');
+            a.style.display = 'none';
+            document.body.appendChild(a);
+            const url = window.URL.createObjectURL(blob);
+            a.href = url;
+            a.download = fileName;
+            a.type = mimeType;
+            a.click();
+            window.URL.revokeObjectURL(url);
+            document.body.removeChild(a);
+        }
+    };
+    xhr.send();
 }
 
 // comment box
@@ -540,18 +546,8 @@ window.buildAjaxAndSubmit = function (url, body, succeededCallback, failedCallba
                     return window.tips(res.message, res.code);
                 }
 
-                $.ajax({
-                    method: 'get',
-                    url: res.data.originalUrl,
-                    success: function (res) {
-                        downloadFile(res, name, mime);
-                    },
-                    complete: function (e) {
-                        btn.prop('disabled', false);
-                        btn.find('.spinner-border').remove();
-                        $('#loading').hide();
-                    },
-                });
+                downloadFile(res.data.originalUrl, name, mime);
+                $('#loading').hide();
             },
             complete: function (e) {
                 btn.prop('disabled', false);
