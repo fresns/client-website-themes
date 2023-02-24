105c6804f9d85b327aaded6cf5e8b8fc708220df
Jarvis <jarvis.okay@gmail.com>
2023-02-24 21:39:07 +0800

    chore(frame): error view

--- ThemeFrame/error.blade.php
+++ ThemeFrame/error.blade.php
@@ -6,8 +6,6 @@
     use App\Helpers\ConfigHelper;
 
     $email = ConfigHelper::fresnsConfigByItemKey('site_email');
-
-    $codeArr = [31501, 31502, 31503, 31504, 31505, 31601, 31602, 31603];
 @endphp
 
 @section('content')
@@ -17,47 +15,11 @@
                 <div class="card-body p-5">
                     <h3 class="card-title">Fresns {{ $code }}</h3>
                     <p>{{ $message }}</p>
-                    <p class="mt-4">Administrator Email: <a href="mailto:{{ $email }}">{{ $email }}</a></p>
-
-                    @if (in_array($code, $codeArr))
-                        <a class="btn btn-outline-success btn-sm mt-4 clear-cookie" href="#" data-method="DELETE" data-action="{{ route('panel.clear.web.cookie') }}">Clear Cookie</a>
+                    @if ($email)
+                        <p class="mt-4">Administrator Email: <a href="mailto:{{ $email }}">{{ $email }}</a></p>
                     @endif
                 </div>
             </div>
         </div>
     </main>
 @endsection
-
-@push('script')
-    <script>
-        /* Fresns Token */
-        $.ajaxSetup({
-            headers: {
-                Accept: 'application/json',
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
-            },
-        });
-
-        $(document).ready(function () {
-            $(document).on('click', '.clear-cookie', function (e) {
-                e.preventDefault();
-                $(this).prop('disabled', true);
-                $(this).prepend(
-                    '<span class="spinner-border spinner-border-sm mg-r-5" role="status" aria-hidden="true"></span> '
-                );
-
-                const url = $(this).data('action'),
-                    type = $(this).data('method') || 'POST',
-                    btn = $(this);
-
-                $.ajax({
-                    url,
-                    type,
-                    complete: function (e) {
-                        location.reload();
-                    },
-                });
-            });
-        });
-    </script>
-@endpush
