3d24222f90785488bf1ac0763eae20eca6399155
Jarvis <jarvis.okay@gmail.com>
2023-03-02 09:16:50 +0800

    chore(frame): interaction mark

--- ThemeFrame/components/comment/mark/block.blade.php
+++ ThemeFrame/components/comment/mark/block.blade.php
@@ -4,7 +4,7 @@
     <input type="hidden" name="markType" value="comment"/>
     <input type="hidden" name="fsid" value="{{ $cid }}"/>
     @if ($interaction['blockStatus'])
-        <a class="dropdown-item py-2 text-success fs-mark" data-interaction-active="{{ $interaction['blockStatus'] }}" data-bi="bi-bookmark-x" data-name="{{ $interaction['blockName'] }}" href="javascript:void(0)">
+        <a class="dropdown-item py-2 text-success fs-mark" data-bi="bi-bookmark-x" data-name="{{ $interaction['blockName'] }}" data-interaction-active="{{ $interaction['blockStatus'] }}" href="javascript:void(0)">
             <i class="bi bi-bookmark-x-fill"></i>
             <span class="show-text">{{ $interaction['blockName'] }}</span>
             @if (fs_api_config('comment_blocker_count'))
--- ThemeFrame/components/post/mark/block.blade.php
+++ ThemeFrame/components/post/mark/block.blade.php
@@ -4,7 +4,7 @@
     <input type="hidden" name="markType" value="post"/>
     <input type="hidden" name="fsid" value="{{ $pid }}"/>
     @if ($interaction['blockStatus'])
-        <a class="dropdown-item py-2 text-success fs-mark" data-interaction-active="{{ $interaction['blockStatus'] }}" data-bi="bi-bookmark-x" data-name="{{ $interaction['blockName'] }}" href="javascript:void(0)">
+        <a class="dropdown-item py-2 text-success fs-mark" data-bi="bi-bookmark-x" data-name="{{ $interaction['blockName'] }}" data-interaction-active="{{ $interaction['blockStatus'] }}" href="javascript:void(0)">
             <i class="bi bi-bookmark-x-fill"></i>
             <span class="show-text">√ {{ $interaction['blockName'] }}</span>
             @if (fs_api_config('post_disliker_count'))
