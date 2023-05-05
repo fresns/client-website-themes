e50e3636b15729228dc6abcc70123ef4d9763a71
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-05-05 23:55:57 +0800

    feat: wallet logs

--- Moments/account/wallet.blade.php
+++ Moments/account/wallet.blade.php
@@ -62,6 +62,59 @@
         <div class="card-header">
             {{ fs_lang('walletLogs') }}
         </div>
-        <div class="card-body"></div>
+        <div class="card-body">
+            <table class="table table-hover align-middle text-nowrap">
+                <thead>
+                    <tr class="table-info">
+                        <th scope="col">{{ fs_lang('walletLogType') }}</th>
+                        <th scope="col">{{ fs_lang('walletLogAmountTotal') }}</th>
+                        <th scope="col">{{ fs_lang('walletLogAmount') }}</th>
+                        <th scope="col">{{ fs_lang('walletLogSystemFee') }}</th>
+                        <th scope="col">{{ fs_lang('walletLogOpeningBalance') }}</th>
+                        <th scope="col">{{ fs_lang('walletLogClosingBalance') }}</th>
+                        <th scope="col">{{ fs_lang('walletLogTime') }}</th>
+                        <th scope="col">{{ fs_lang('walletLogRemark') }}</th>
+                        <th scope="col">{{ fs_lang('walletLogUser') }}</th>
+                        <th scope="col">{{ fs_lang('walletLogStatus') }}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($logs as $log)
+                        <tr>
+                            <th scope="row">{{ fs_lang("walletLogType{$log['type']}") }}</th>
+                            <td>{{ $log['amountTotal'] }}</td>
+                            <td>{{ $log['transactionAmount'] }}</td>
+                            <td>{{ $log['systemFee'] }}</td>
+                            <td>{{ $log['openingBalance'] }}</td>
+                            <td>{{ $log['closingBalance'] }}</td>
+                            <td>
+                                <time class="text-secondary" datetime="{{ $log['createdDatetime'] }}" data-bs-toggle="tooltip" data-bs-placement="bottom" title="{{ $log['createdDatetime'] }}">
+                                    {{ $log['createdTimeAgo'] }}
+                                </time>
+                            </td>
+                            <td>{{ $log['remark'] }}</td>
+                            <td>
+                                @if ($log['user'])
+                                    @if ($log['user']['status'])
+                                        <a href="{{ fs_route(route('fresns.profile.index', ['uidOrUsername' => $log['user']['fsid']])) }}">
+                                            <img src="{{ $log['user']['avatar'] }}" loading="lazy" class="rounded-circle" width="24" height="24">
+                                            {{ $log['user']['nickname'] }}
+                                        </a>
+                                    @else
+                                        {{-- Deactivate --}}
+                                        <img src="{{ fs_api_config('deactivate_avatar') }}" loading="lazy" alt="{{ fs_lang('userDeactivate') }}" class="user-avatar rounded-circle">
+                                    @endif
+                                @endif
+                            </td>
+                            <td><i class="bi {{ $log['status'] ? 'bi-check-lg text-success' : 'bi-dash-lg text-secondary' }}"></i></td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
+            <div class="my-3 table-responsive">
+                {{ $logs->links() }}
+            </div>
+        </div>
     </div>
 @endsection
