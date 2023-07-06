3c48440717e63115df55d9c7749e34b3e4550070
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-07-06 11:50:03 +0800

    fix: lang key

--- ThemeFrame/components/editor/tip/edit.blade.php
+++ ThemeFrame/components/editor/tip/edit.blade.php
@@ -1,13 +1,13 @@
 <div class="editor-alert">
     @if ($config['editableStatus'])
         <div class="alert alert-danger" role="alert">
-            {{ fs_lang('editoreditedDatetimeTitle') }}<br>
-            <kbd>{{ fs_lang('editoreditedDatetimeDesc') }}: {{ $config['editableTime'] }} ({{ $config['deadlineTime'] }})</kbd>
+            {{ fs_lang('editorEditTimeTitle') }}<br>
+            <kbd>{{ fs_lang('editorEditTimeDesc') }}: {{ $config['editableTime'] }} ({{ $config['deadlineTime'] }})</kbd>
         </div>
     @else
         <div class="alert alert-danger" role="alert">
             <kbd>{{ fs_lang('errorTimeout') }}</kbd>
-            {{ fs_lang('editoreditedDatetimeTitle') }}
+            {{ fs_lang('editorEditTimeTitle') }}
         </div>
     @endif
 </div>
