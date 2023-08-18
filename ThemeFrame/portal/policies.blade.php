57786b330852c5574403c78cb8c2d6cdea6ff34c
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-08-18 13:53:01 +0800

    fix: policies tab

--- ThemeFrame/portal/policies.blade.php
+++ ThemeFrame/portal/policies.blade.php
@@ -11,40 +11,40 @@
                 </div>
     
                 @if (fs_api_config('account_terms_status'))
-                    <button class="nav-link text-start rounded-0 py-3 active" id="terms-tab" data-bs-toggle="pill" data-bs-target="#terms" type="button" role="tab" aria-controls="terms" aria-selected="true">{{ fs_lang('accountPoliciesTerms') }}</button>
+                    <button class="nav-link text-start rounded-0 py-3 active" id="terms-tab" data-bs-toggle="pill" data-bs-target="#account-terms" type="button" role="tab" aria-controls="terms" aria-selected="true">{{ fs_lang('accountPoliciesTerms') }}</button>
                 @endif
 
                 @if (fs_api_config('account_privacy_status'))
-                    <button class="nav-link text-start rounded-0 py-3" id="privacy-tab" data-bs-toggle="pill" data-bs-target="#privacy" type="button" role="tab" aria-controls="privacy" aria-selected="false">{{ fs_lang('accountPoliciesPrivacy') }}</button>
+                    <button class="nav-link text-start rounded-0 py-3" id="privacy-tab" data-bs-toggle="pill" data-bs-target="#account-privacy" type="button" role="tab" aria-controls="privacy" aria-selected="false">{{ fs_lang('accountPoliciesPrivacy') }}</button>
                 @endif
 
                 @if (fs_api_config('account_cookies_status'))
-                    <button class="nav-link text-start rounded-0 py-3" id="cookies-tab" data-bs-toggle="pill" data-bs-target="#cookies" type="button" role="tab" aria-controls="cookies" aria-selected="false">{{ fs_lang('accountPoliciesCookies') }}</button>
+                    <button class="nav-link text-start rounded-0 py-3" id="cookies-tab" data-bs-toggle="pill" data-bs-target="#account-cookies" type="button" role="tab" aria-controls="cookies" aria-selected="false">{{ fs_lang('accountPoliciesCookies') }}</button>
                 @endif
 
                 @if (fs_api_config('account_delete_status'))
-                    <button class="nav-link text-start rounded-0 py-3" id="delete-tab" data-bs-toggle="pill" data-bs-target="#delete" type="button" role="tab" aria-controls="delete" aria-selected="false">{{ fs_lang('accountDelete') }}</button>
+                    <button class="nav-link text-start rounded-0 py-3" id="delete-tab" data-bs-toggle="pill" data-bs-target="#account-delete" type="button" role="tab" aria-controls="delete" aria-selected="false">{{ fs_lang('accountDelete') }}</button>
                 @endif
             </div>
 
             <div class="tab-content border-start ps-lg-5 pb-5 account-settings" id="policies-tab-content">
                 {{-- Terms --}}
-                <div class="tab-pane fade show active" id="terms" role="tabpanel" aria-labelledby="terms-tab" tabindex="0">
+                <div class="tab-pane fade show active" id="account-terms" role="tabpanel" aria-labelledby="terms-tab" tabindex="0">
                     {!! fs_api_config('account_terms') ? Str::markdown(fs_api_config('account_terms')) : '' !!}
                 </div>
 
                 {{-- Privacy --}}
-                <div class="tab-pane fade" id="privacy" role="tabpanel" aria-labelledby="privacy-tab" tabindex="0">
+                <div class="tab-pane fade" id="account-privacy" role="tabpanel" aria-labelledby="privacy-tab" tabindex="0">
                     {!! fs_api_config('account_privacy') ? Str::markdown(fs_api_config('account_privacy')) : '' !!}
                 </div>
 
                 {{-- Cookies --}}
-                <div class="tab-pane fade" id="cookies" role="tabpanel" aria-labelledby="cookies-tab" tabindex="0">
+                <div class="tab-pane fade" id="account-cookies" role="tabpanel" aria-labelledby="cookies-tab" tabindex="0">
                     {!! fs_api_config('account_cookies') ? Str::markdown(fs_api_config('account_cookies')) : '' !!}
                 </div>
 
                 {{-- Delete Account --}}
-                <div class="tab-pane fade" id="delete" role="tabpanel" aria-labelledby="delete-tab" tabindex="0">
+                <div class="tab-pane fade" id="account-delete" role="tabpanel" aria-labelledby="delete-tab" tabindex="0">
                     {!! fs_api_config('account_delete') ? Str::markdown(fs_api_config('account_delete')) : '' !!}
                 </div>
             </div>
